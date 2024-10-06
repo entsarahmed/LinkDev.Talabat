8bb4c2f17c677c307b7337c7ae2f750fbb320018
entsarahmed <entesara500@gmail.com>
2024-10-06 21:41:21 +0300

    Apply Migrations (Update-Database)

--- LinkDev.Talabat.APIs/Program.cs
+++ LinkDev.Talabat.APIs/Program.cs
@@ -1,5 +1,6 @@
 
 using LinkDev.Talabat.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
@@ -7,10 +8,14 @@ namespace LinkDev.Talabat.APIs
 {
     public class Program
     {
-        public static void Main(string[] args)
+       // public static StoreContext StoreContext { get; set; } = null!;
+        // Entry Point
+        public static async Task Main(string[] args)
         {
-            var webApplicationBuilder = WebApplication.CreateBuilder(args);
 
+            var webApplicationBuilder = WebApplication.CreateBuilder(args);
+            
+            
             #region Configure Services
             // Add services to the container.
 
@@ -23,13 +28,59 @@ namespace LinkDev.Talabat.APIs
             webApplicationBuilder.Services.AddSwaggerGen();
 
 
-            webApplicationBuilder.Services.AddPersistenceServices(webApplicationBuilder.Configuration); 
+            webApplicationBuilder.Services.AddPersistenceServices(webApplicationBuilder.Configuration);
             //DependenecyInjection.AddPersistenceServices(webApplicationBuilder.Services,webApplicationBuilder.Configuration);
+            #endregion
+
+            var app = webApplicationBuilder.Build();
+
+            #region implicitly want to clr create object from class DbContext
+            ////try
+            ////{
+            ////    var PendingMigrations = StoreContext.Database.GetPendingMigrations();
+
+            ////    if(PendingMigrations.Any()) 
+            ////          await  StoreContext.Database.MigrateAsync(); //Update-Database
+            ////}
+            ////catch(Exception ex) 
+            ////{
+
+            ////}
+            ////finally
+            ////{
+            //// await   StoreContext.DisposeAsync();
+            ////} 
+            #endregion
+
+
+            #region Update Database
+
+            using var scope = app.Services.CreateAsyncScope();
+            var services = scope.ServiceProvider;
+
+            var dbContext = services.GetRequiredService<StoreContext>();
+            //Ask Runtime Env for an Object from "StoreContext" Service Explicitly.
+
+            var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+            //var logger = services.GetRequiredService<ILogger<Program>>();
+
+            try
+            {
+                var PendingMigrations = dbContext.Database.GetPendingMigrations();
+
+                if (PendingMigrations.Any())
+                    await dbContext.Database.MigrateAsync(); //Update-Database
+            }
+            catch (Exception ex)
+            {
+                var logger = loggerFactory.CreateLogger<Program>();
+                logger.LogError(ex, "an error has been occured during applying the migrations");
+            }
+
             #endregion
 
 
             #region Cofigure Kestrel Middleware
-            var app = webApplicationBuilder.Build();
 
             // Configure the HTTP request pipeline.
 
