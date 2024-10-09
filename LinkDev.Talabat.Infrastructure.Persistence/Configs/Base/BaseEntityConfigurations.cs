87d7a11507081bf698469a2721aa8b9e8a70c05b
entsarahmed <entesara500@gmail.com>
2024-10-09 08:10:20 +0300

    BaseEntity and BaseAuditableEntity

--- LinkDev.Talabat.Infrastructure.Persistence/Configs/Base/BaseEntityConfigurations.cs
+++ LinkDev.Talabat.Infrastructure.Persistence/Configs/Base/BaseEntityConfigurations.cs
@@ -1,6 +1,5 @@
 ﻿using LinkDev.Talabat.Core.Domain.Common;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,23 +12,11 @@ namespace LinkDev.Talabat.Infrastructure.Persistence.Configs.Base
     {
         public virtual void Configure(EntityTypeBuilder<TEntity> builder)
         {
-
             builder.Property(E => E.Id)
-             .IsRequired()
-             .ValueGeneratedOnAdd();
-
-            builder.Property(E => E.CreatedBy)
-                .IsRequired();
-            builder.Property(E => E.CreatedOn)
-                .IsRequired()
-               /* .HasDefaultValue("GetUTCDate()")*/;
-
-
-            builder.Property(E => E.LastModifiedBy)
-               .IsRequired();
-            builder.Property(E => E.LastModifiedOn)
-                .IsRequired()
-                /*.HasDefaultValue("GetUTCDate()")*/;
+                        .IsRequired()
+                        .ValueGeneratedOnAdd();
         }
+
+
     }
 }
