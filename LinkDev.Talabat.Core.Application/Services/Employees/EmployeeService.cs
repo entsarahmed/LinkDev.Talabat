4df23a6db45a4f9e0f0ced1bee64ce9905e4dc6e
entsarahmed <entesara500@gmail.com>
2024-10-10 11:30:14 +0300

    Employee Example -Draft

---
+++ LinkDev.Talabat.Core.Application/Services/Employees/EmployeeService.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using LinkDev.Talabat.Core.Application.Abstraction.Services.Employees;
+using LinkDev.Talabat.Core.Domain.Contracts;
+using LinkDev.Talabat.Core.Domain.Entities.Employees;
+using LinkDev.Talabat.Core.Domain.Specifications.Employees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Core.Application.Services.Employees
+{
+    internal class EmployeeService(IUnitOfWork unitOfWork, IMapper mapper) : IEmployeeService
+    {
+        public async Task<IEnumerable<EmployeesToReturnDto>> GetEmployeeAsync()
+        {
+            var spec = new EmployeeWithDepartmentSpecifications();
+           var employees = await unitOfWork.GetRepository<Employee, int>().GetAllWithSpecAsync(spec);
+            var employeesToReturn = mapper.Map<IEnumerable<EmployeesToReturnDto>>(employees);
+            return employeesToReturn;
+        }
+
+        public async Task<EmployeesToReturnDto> GetEmployeeAsync(int id)
+        {
+            var spec = new EmployeeWithDepartmentSpecifications(id);
+            var employee = await unitOfWork.GetRepository<Employee, int>().GetAllWithSpecAsync(spec);
+            var employeesToReturn = mapper.Map<EmployeesToReturnDto>(employee);
+            return employeesToReturn;
+
+        }
+    }
+}
---
+++ LinkDev.Talabat.Core.Domain/Specifications/Employees/EmployeeWithDepartmentSpecifications.cs
@@ -0,0 +1,17 @@
+﻿using LinkDev.Talabat.Core.Domain.Entities.Employees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Core.Domain.Specifications.Employees
+{
+    public class EmployeeWithDepartmentSpecifications : BaseSpecifications<Employee,int>
+    {
+        public EmployeeWithDepartmentSpecifications()
+        {
+            Includes.Add(E => E.Department!);
+        }
+    }
+}
