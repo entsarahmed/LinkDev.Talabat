c2468346a53ac4e04042626f13493cf70c2f8245
entsarahmed <entesara500@gmail.com>
2024-10-10 11:51:09 +0300

    Product Url Resolver

--- LinkDev.Talabat.Core.Application/Mapping/MappingProfile.cs
+++ LinkDev.Talabat.Core.Application/Mapping/MappingProfile.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Castle.Core.Configuration;
 using LinkDev.Talabat.Core.Application.Abstraction.Models.Products;
 using LinkDev.Talabat.Core.Domain.Entities.Products;
 using System;
@@ -11,11 +12,12 @@ namespace LinkDev.Talabat.Core.Application.Mapping
 {
     internal class MappingProfile : Profile
     {
-        public MappingProfile()
+        public MappingProfile( IConfiguration configuration)
         {
             CreateMap<Product, ProductToReturnDto>()
                 .ForMember(d => d.Brand, O => O.MapFrom(src => src.Brand!.Name))
-                .ForMember(d => d.Category, O => O.MapFrom(src => src.Category!.Name));
+                .ForMember(d => d.Category, O => O.MapFrom(src => src.Category!.Name))
+                .ForMember(d => d.PictureUrl, O => O.MapFrom(S => $"{"https://localhost:7033}{S.PictureUrl}") );
 
             CreateMap<ProductBrand, BrandDto>();
 
