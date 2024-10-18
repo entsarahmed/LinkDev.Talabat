ea8157688ebfa350d017c798f1340f6334d9200b
entsarahmed <entesara500@gmail.com>
2024-10-18 13:57:10 +0300

    Identity  Module  -  Entities

---
+++ LinkDev.Talabat.Core.Domain/Entities/Identity/Address.cs
@@ -0,0 +1,25 @@
+﻿using LinkDev.Talabat.Core.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Core.Domain.Entities.Identity
+{
+    public class Address 
+    {
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Street { get; set; }
+        public required string City { get; set; }
+        public required string  Country { get; set; }
+
+        public int UserId { get; set; }
+        public required ApplicationUser AppUser { get; set; }
+
+
+
+    }
+}
