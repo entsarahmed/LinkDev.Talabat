f4e43cdb4082a76c0f14afe5f70746ef37937647
entsarahmed <entesara500@gmail.com>
2024-11-22 16:10:57 +0200

    Order Module - Entities

---
+++ LinkDev.Talabat.Core.Domain/Entities/Orders/OrderItem.cs
@@ -0,0 +1,20 @@
+﻿using LinkDev.Talabat.Core.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Core.Domain.Entities.Orders
+{
+    public class OrderItem : BaseAuditableEntity<int>
+    {
+
+        public required ProductItemOrdered Product { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+
+
+    }
+}
