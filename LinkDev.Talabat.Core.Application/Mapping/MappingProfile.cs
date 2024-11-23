c2196ee6b0c0112af398bf332264694291b8e00b
entsarahmed <entesara500@gmail.com>
2024-11-23 10:12:38 +0200

    Order Service and Dtos

--- LinkDev.Talabat.Core.Application/Mapping/MappingProfile.cs
+++ LinkDev.Talabat.Core.Application/Mapping/MappingProfile.cs
@@ -1,10 +1,13 @@
 ﻿using AutoMapper;
 using Castle.Core.Configuration;
+using LinkDev.Talabat.Core.Application.Abstraction.Models._Common;
 using LinkDev.Talabat.Core.Application.Abstraction.Models.Basket;
+using LinkDev.Talabat.Core.Application.Abstraction.Models.Orders;
 using LinkDev.Talabat.Core.Application.Abstraction.Models.Products;
 using LinkDev.Talabat.Core.Application.Abstraction.Services.Employees;
 using LinkDev.Talabat.Core.Domain.Entities.Basket;
 using LinkDev.Talabat.Core.Domain.Entities.Employees;
+using LinkDev.Talabat.Core.Domain.Entities.Orders;
 using LinkDev.Talabat.Core.Domain.Entities.Products;
 using System;
 using System.Collections.Generic;
@@ -38,7 +41,15 @@ namespace LinkDev.Talabat.Core.Application.Mapping
             CreateMap<CustomerBasket, CustomerBasketDto>().ReverseMap();
             CreateMap<BasketItem, BasketItemDto>().ReverseMap();
 
+            CreateMap<Order, OrderToReturnDto>()
+                .ForMember(dest => dest.DeliveryMethod, options => options.MapFrom(src => src.DeliveryMethod!.ShortName));
 
+            CreateMap<OrderItem, OrderItemDto>()
+                .ForMember(dest => dest.ProductId, options => options.MapFrom(src => src.Product.ProductId))
+                .ForMember(dest => dest.ProductName, options => options.MapFrom(src => src.Product.ProductName))
+                .ForMember(dest => dest.PictureUrl, options => options.MapFrom<OrderItemPictureUrlResolver>());
+            CreateMap<Address, AddressDto>();
+            CreateMap<DeliveryMethod, DeliveryMethodDto>();
         }
     }
 }
---
+++ LinkDev.Talabat.Core.Application/Mapping/OrderItemPictureUrlResolver.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using LinkDev.Talabat.Core.Application.Abstraction.Models.Orders;
+using LinkDev.Talabat.Core.Application.Abstraction.Models.Products;
+using LinkDev.Talabat.Core.Domain.Entities.Orders;
+using LinkDev.Talabat.Core.Domain.Entities.Products;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace LinkDev.Talabat.Core.Application.Mapping
+{
+    internal class OrderItemPictureUrlResolver(IConfiguration configuration) : IValueResolver<OrderItem, OrderItemDto, string>
+    {
+        
+        public string Resolve(OrderItem source, OrderItemDto destination, string? destMember, ResolutionContext context)
+        {
+            if (!string.IsNullOrEmpty(source.Product.PictureUrl))
+                //var apiBaseUrl = configuration.GetValue<string>("Urls:ApiBaseUrl");
+                //return $"{apiBaseUrl}{source.PictureUrl}";
+                 return $"{configuration["Urls:ApiBaseUrl"]}/{source.Product.PictureUrl}";
+
+            return string.Empty;
+
+        }
+    }
+}
