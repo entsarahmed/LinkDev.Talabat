8fb4d659a44adb20f3d7391679b8c2914dcac579
entsarahmed <entesara500@gmail.com>
2024-12-10 11:44:20 +0200

    Setting Stripe Account

---
+++ LinkDev.Talabat.Core.Domain/Contracts/Infrastructure/IPaymentService.cs
@@ -0,0 +1,16 @@
+﻿using LinkDev.Talabat.Core.Domain.Entities.Basket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Core.Domain.Contracts.Infrastructure
+{
+    public interface IPaymentService
+    {
+
+        Task<CustomerBasket?> CreateOrUpdatePaymentIntent(string basketId);
+
+    }
+}
---
+++ LinkDev.Talabat.Infrastructure/Payment Service/PaymentService.cs
@@ -0,0 +1,18 @@
+﻿using LinkDev.Talabat.Core.Domain.Contracts.Infrastructure;
+using LinkDev.Talabat.Core.Domain.Entities.Basket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Infrastructure.Payment_Service
+{
+    public class PaymentService : IPaymentService
+    {
+        public Task<CustomerBasket?> CreateOrUpdatePaymentIntent(string basketId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
